bd68bf18904104ff4d2625f5e278690928f840db
Rahul Somasundaram <Rahul.Somasundaram@checkpt.com>
2022-12-29 11:43:00 +0530

    initial commit

---
+++ src/main/java/xyz/ssrahul96/springbootecho/SpringBootEchoApplication.java
@@ -0,0 +1,13 @@
+package xyz.ssrahul96.springbootecho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEchoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEchoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/ssrahul96/springbootecho/SpringBootEchoApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.ssrahul96.springbootecho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEchoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
