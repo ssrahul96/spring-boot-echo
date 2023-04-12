22377a382612bd354506e50440380e06e775e11b
Rahul Somasundaram <Rahul.Somasundaram@checkpt.com>
2023-04-12 12:24:19 +0530

    added simulate option at startup

--- src/main/java/xyz/ssrahul96/springbootecho/SpringBootEchoApplication.java
+++ src/main/java/xyz/ssrahul96/springbootecho/SpringBootEchoApplication.java
@@ -1,13 +1,32 @@
 package xyz.ssrahul96.springbootecho;
 
+import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.system.SystemProperties;
 
 @SpringBootApplication
+@Log4j2
 public class SpringBootEchoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootEchoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        simulateDelay();
+        SpringApplication.run(SpringBootEchoApplication.class, args);
+    }
 
+    private static void simulateDelay() {
+        int delayInSec = NumberUtils.toInt(SystemProperties.get("STARTUP_DELAY_SEC"));
+
+        if (delayInSec == 0) {
+            return;
+        }
+
+        try {
+            log.info("simulating delay for {} seconds", delayInSec);
+            Thread.sleep(delayInSec * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
